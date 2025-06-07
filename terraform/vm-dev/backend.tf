03faeac9cd7f8a67cb95e7537fa5c0500bc6b5f8
femymellwin <54503706+femymellwin@users.noreply.github.com>
2025-06-08 01:28:06 +0530

    Update backend.tf

--- terraform/vm-dev/backend.tf
+++ terraform/vm-dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "myproject-terraform-state-bucket"
+    bucket         = "fmshr-tfe-statefile-bucket"
     key            = "vm/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
