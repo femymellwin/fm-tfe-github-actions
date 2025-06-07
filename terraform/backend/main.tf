2298f8854e3883d0f0d7d57dc30f9969cd78950f
femymellwin <54503706+femymellwin@users.noreply.github.com>
2025-06-08 00:50:10 +0530

    Create main.tf

---
+++ terraform/backend/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "myproject-terraform-state-bucket"
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
